2495173583ac4f513c00b5b7dd18cf009aae68fd
DanSjukdom <daniel.py.216@gmail.com>
2020-08-19 18:39:13 -0500

    Event api in urls

--- djangokalendar/djkalendar/urls.py
+++ djangokalendar/djkalendar/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import Index
+from .views import Index, EventAPI, EventDetailAPI
 
 
 app_name = 'djkalendar'
 
 urlpatterns = [
-	path('', Index.as_view(), name='index')	
+	path('', Index.as_view(), name='index'),
+	path('api/events/', EventAPI.as_view(), name='api-events'),
+	path('api/events/<int:id>/', EventDetailAPI.as_view(), name='api-events-detail')
 ]
